7eabf3382be7713c79cd39edf004ead0bae036da
mikethreels <michaelthreels@gmail.com>
2020-07-24 20:04:14 +0200

    added friendship arrray

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,22 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    confirmed_friends
   end
 
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+  end
+
+  def confirmed_friends
+    @friend_arr = current_user.friendships.pluck(:friend_id, :status) + current_user.reverse_friendships.pluck(:user_id, :status)
+    @friend_arr.select! do | i |
+      i.pop if i[1] == true
+    end
+    @friend_arr.flatten!
+  end
+  def invite
+
   end
 end
