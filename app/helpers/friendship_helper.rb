25ee7ba01110aa438b7034bbfde39fdce0618d46
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-07-28 09:57:03 -0500

    Fixed rubo offenses

--- app/helpers/friendship_helper.rb
+++ app/helpers/friendship_helper.rb
@@ -1,6 +1,10 @@
 module FriendshipHelper
   def friend_search
-    all_friend_arr = current_user.friendships.pluck(:friend_id, :status) + current_user.reverse_friendships.pluck(:user_id, :status)
+    user_friends = current_user.friendships.pluck(:friend_id, :status)
+    inverse_friends = current_user.reverse_friendships.pluck(:user_id, :status)
+
+    all_friend_arr = user_friends + inverse_friends
+
     temporary_friends = []
     all_friend_arr.select do |friend|
       temporary_friends << friend[0] if yield(friend)
@@ -13,8 +17,7 @@ module FriendshipHelper
   end
 
   def invite_button(user_id)
-    unless (@confirmed_friends + @pending_friends).include?(user_id) || current_user.id == user_id
-      (button_to 'invite', friendships_path, method: :post, params: { friend_id: user_id }).html_safe
-      end
+    button = (button_to 'invite', friendships_path, method: :post, params: { friend_id: user_id }).html_safe
+    return button unless (@confirmed_friends + @pending_friends).include?(user_id) || current_user.id == user_id
   end
 end
